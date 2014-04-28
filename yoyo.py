5e487906f59f80fdc23dee180195e6ea88d61063
John Crawford <john.crawford@crawfishpress.com>
2014-04-28 10:06:33 -0400

    Update yoyo.py

--- yoyo.py
+++ yoyo.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/yoyo")
 def hello():
-    status =  "Project Yoyo: it's up, it's down - who knows where it will end up! (V0.9)"
+    status =  "Project Yoyo: it's up, it's down - who knows where it will end up! (V0.88)"
     return status
 
 if __name__ == "__main__":
